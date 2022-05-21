27818e2535c13407faf74a5a448b4a85447ebd8e
Dani Coleman <danicoleman00@gmail.com>
2022-05-21 13:27:33 -0600

    complete user story 3

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -16,4 +16,29 @@ RSpec.describe 'Project Show Page' do
       expect(page).to have_content("Challenge Theme: Recycled Material")
     end
   end
+
+  context 'User Story 3' do
+    it "can see a count of the number of contestants on this project" do
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+
+      visit "/projects/#{news_chic.id}"
+
+      expect(page).to have_content("News Chic")
+      expect(page).to have_content("Material: Newspaper")
+      expect(page).to have_content("Challenge Theme: Recycled Material")
+
+      expect(page).to have_content("Number of Contestants: 2")
+      expect(page).to_not have_content("Number of Contestants: 3")
+    end
+  end
 end
