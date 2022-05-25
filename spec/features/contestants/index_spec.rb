9a4f6573d6ed1c862a3cca79bfbbd9d558fe255b
Dani Coleman <danicoleman00@gmail.com>
2022-05-25 07:03:27 -0600

    remove ext2 test from contestant index

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -95,45 +95,4 @@ RSpec.describe 'Contestants Index Page' do
       end
     end
   end
-
-  context 'Extension 2' do
-    it "can see a project after being added to it" do
-      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
-
-      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-      boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
-      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-
-      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-      gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-      kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
-      erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
-
-      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-
-      ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
-      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
-
-      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
-      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
-
-      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
-
-      visit "/projects/#{news_chic.id}"
-
-      fill_in :contestant_id, with: erin.id
-
-      click_on "Add Contestant To Project"
-
-      visit '/contestants'
-
-      within("#contestant-#{erin.id}") do
-        expect(page).to have_content("Erin Robertson")
-        expect(page).to_not have_content("Gretchen Jones")
-        expect(page).to have_content("Projects: News Chic, Boardfit")
-        expect(page).to_not have_content("Projects: Upholstery Tuxedo")
-      end
-    end
-  end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -71,9 +71,11 @@ RSpec.describe 'Project Show Page' do
   context 'Extenstion 2' do
     it "can see a form to add a contestant to this project" do
       recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
 
       news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
       boardfit = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
 
       jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
       gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
@@ -81,7 +83,14 @@ RSpec.describe 'Project Show Page' do
       erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
       ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+
       ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+
+      ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+      ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+
+      ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
 
       visit "/projects/#{news_chic.id}"
 
@@ -96,6 +105,12 @@ RSpec.describe 'Project Show Page' do
       expect(current_path).to eq("/projects/#{news_chic.id}")
       expect(page).to have_content("Number of Contestants: 3")
       expect(page).to have_content("Average Contestant Experience: 13.33 years")
+
+      visit '/contestants'
+      
+      within("#contestant-#{erin.id}") do
+        expect(page).to have_content("Projects: News Chic, Boardfit")
+      end
     end
   end
 end
